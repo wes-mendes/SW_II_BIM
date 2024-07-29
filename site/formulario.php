cf980f92206a9323aff1407ee71fadcdb7b90f9c
Wesley <124800998+wes-mendes@users.noreply.github.com>
2024-07-29 09:01:26 -0300

    Add files via upload

---
+++ site/formulario.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    
+<form action="processa.php" method="GET">
+  <label for="nome">Nome:</label><br>
+  <input type="text" name="nome"><br>
+  <label for="idade">Idade:</label><br>
+  <input type="text" name="idade"><br>
+  <label for="email">Email:</label><br>
+  <input type="text" name="email"><br>
+  <input type="submit" value="ENVIAR">
+</form> 
+
+
+</body>
+</html>
---
+++ site/processa.php
@@ -0,0 +1,14 @@
+<?php
+    $usuario = $_REQUEST['usuario'];
+    $senha = $_REQUEST['senha'];
+
+    echo "Olá <span style='color:red;'>" . $usuario . "</span>";
+    echo "<br>";
+    echo "Você tem $idade anos de vida";
+    echo "<br>";
+    echo "Seu email é : " . $email;
+    echo "<br>";
+
+
+
+?>
