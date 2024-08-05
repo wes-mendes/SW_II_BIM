1efdbd02b073360d6400766cdc560299851fb83b
Wesley <124800998+wes-mendes@users.noreply.github.com>
2024-08-05 09:03:47 -0300

    Add files via upload

---
+++ site_2c_turmaB/painel/processa.php
@@ -0,0 +1,16 @@
+<?php
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "cadastro_cliente";
+
+    $conexao = new mysqli($servidor, $usuario, $senha, $banco);
+
+    if(mysqli_connect_errno()){
+        echo "ERRO DE CONEXÃO!";
+    }
+    //else{
+        //echo "CONECTADO AO BANCO COM SUCESSO!";
+    //}
+
+?>
